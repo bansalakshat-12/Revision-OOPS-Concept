0997e77d8a41827186a7d9fc415e8530ea5f4d2c
Akshat <akshatbansal217@gmail.com>
2025-10-31 15:56:24 +0530

    Signin and signup feature added

--- oops_proj.py
+++ oops_proj.py
@@ -8,24 +8,50 @@ class chatbook:
         
     def menu(self):
         user_input = input("""Welcome to the chatbook! How would you like to proceed?
-                           1. Press 1 to Signup
-                           2. Press 2 to Signin
-                           3. Press 3 to write a post
-                           4. Press 4 to message a friend 
-                           5. Press any other key to exit""")
-        if user_input == 1:
+                           1. Press 1 to Signup..
+                           2. Press 2 to Signin..
+                           3. Press 3 to write a post..
+                           4. Press 4 to message a friend.. 
+                           5. Press any other key to exit..""")
+        if user_input == "1":
+            self.signup()
+        
+        elif user_input == "2":
+            self.signin()
+        
+        elif user_input == "3":
             pass
         
-        elif user_input == 2:
-            pass
-        
-        elif user_input == 3:
-            pass
-        
-        elif user_input == 4:
+        elif user_input == "4":
             pass
         
         else:
             exit()
             
+    def signup(self):
+        email = input("Write your email here : ")
+        pwd = input("Enter your password here : ")
+        self.username = email
+        self.password = pwd     
+        print("You Have signedup sucessfully!")
+        print("\n")
+        self.menu()        
+        
+        
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Please signup first by pressing 1 in the main menu..")
+            
+        else:
+            uname = input("Enter the email/username here : ")
+            pwd = input("Enter the password : ")
+            if self.username == uname and self.password==pwd:
+                print("You have signedin sucessfully..")
+                self.logegin = True
+                
+            else:
+                print("Please input correct credentials..")
+        print("\n")
+        self.menu()
+        
 obj = chatbook()
